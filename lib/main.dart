fbaeaaf835ca096d1c551083ee0132aed052e8fe
shimizu-saffle <shimizu.saffle@gmail.com>
2022-08-27 21:08:06 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TravelPlanner());
+}
+
+class TravelPlanner extends StatelessWidget {
+  const TravelPlanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
