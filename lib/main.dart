c6f7e6db4683344b44117ed551ac053d46a31dd1
shimizu-saffle <shimizu.saffle@gmail.com>
2022-08-28 18:28:23 +0900

    add assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_planner_ui_sample/pages/trip_plan_page.dart';
 
 void main() {
   runApp(const TravelPlanner());
@@ -10,7 +11,7 @@ class TravelPlanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(),
+      home: TripPlanPage(),
     );
   }
 }
